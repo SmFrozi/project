da9317660c0edfb12ad4ad558ea352a98da1dbe0
Deividas <xikdron9@gmail.com>
2024-07-15 09:39:01 +0300

    another commit

--- js/script.js
+++ js/script.js
@@ -297,3 +297,82 @@
 //         }
 //     }
 // }
+
+// const numberOfFilms = +prompt("Сколько ты фильмов посмотрел", "");
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt("Один из последних просмотренных фильмов?", ""),
+//         b = prompt("На сколько оцените его?", "");
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('Error');
+//         i--;
+//     }
+
+//     personalMovieDB.movies[a] = b;
+// }
+
+// if (personalMovieDB.count < 10) {
+//     console.log('Просмотрено довольно мало фильмов');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log('ВЫ классический зритель');
+// } else if (personalMovieDB.count >= 30) {
+//     console.log("Вы киноман");
+// } else {
+//     console.log("Произошла ошибка!");
+// }
+
+
+
+// console.log(personalMovieDB);
+
+// function showFirstMessage() {
+//     console.log("Hello world!");
+// }
+
+// showFirstMessage();
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     num = 10;
+// }
+
+// showFirstMessage("Hello world!");
+// console.log(num);
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+// function ret() {
+//     let num = 50;
+
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function () {
+//     console.log("Hello!")
+// };
+
+// logger();
+
+// const calc = (a, b) => { return a + b };
